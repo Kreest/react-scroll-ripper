6ff2e2c3ef1bb39409cc679ddc802c3d1518b5b7
kreest <kreest503@gmail.com>
2021-12-18 17:16:09 +0100

    First working version

--- demo/components/App.jsx
+++ demo/components/App.jsx
@@ -5,13 +5,22 @@ export default class App extends React.Component {
     constructor() {
         super();
 
-        this.state = {a:null};
+        this.state = {};
     }
     render(){
-        return (<ScrollRipper height={3000} offsets={[1000,2000, 3000]}>
-            <div style={{position:"sticky", top:0, height: "100vh", background: "blue"}}> Hello </div>
-            <div style={{position:"sticky", top:0, height: "100vh", background: "red"}}> There</div>
-            <div style={{position:"sticky", top:0, height: "100vh", background: "green"}}> General Kenobi!</div>
+        return (
+            <ScrollRipper height={8000} offsets={[2000,4000,6000,8000]}>
+                <div style={{height: "100vh", width: "100vw",  background: "blue"}}> Hello </div>
+                <div style={{height: "100vh", width: "100vw",  background: "red"}}> There</div>
+                <div style={{height: "100vh", width: "100vw", background: "green"}}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                </div>
+                <div style={{height: "100vh", width: "100vw", background: "purple"}}> I have the high ground!</div>
             </ScrollRipper>);
     }
 }
--- lib/ScrollRipper.jsx
+++ lib/ScrollRipper.jsx
@@ -3,22 +3,27 @@ import React from 'react';
 export default class ScrollRipper extends React.Component {
     constructor() {
         super();
-
         this.state = {sy:0}
     }
     
     render(){
         const {offsets, children} = this.props;
         const ci = offsets.findIndex((o) => this.state.sy <= o);
+        const diff = offsets[ci] - this.state.sy ;
         // const fc = children.map((c) => {
         //     c
         // });
-        console.log(ci);
+        console.log(diff);
 
-        return <div style={{height: this.props.height}}> {children[ci]} </div>;
+        return (
+            <div style={{height: this.props.height}}>
+                <div style={{position: "fixed", top: Math.min(0, diff - window.innerHeight)}}>{children[ci]}</div>
+                <div style={{position: "fixed", top: diff + "px"}}>{children[ci+1]}</div> 
+            </div>
+        )
     }
 
     componentDidMount(){
-        window.addEventListener("scroll", (_) => {this.setState({sy:window.scrollY}); console.log(window.scrollY)})
+        window.addEventListener("scroll", (_) => {this.setState({sy:window.scrollY})})
     }
 }
